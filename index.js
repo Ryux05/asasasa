2b63b2f6658436f4112e842cd00d0e4865ca23a2
yyyyyy <youindo10@gmail.com>
2024-10-07 09:21:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // server.js
 const express = require("express");
-const scrapeAndSendKey = require("./mmH.js"); // Import the scraper function
+const scrapeAndSendKey = require("./mm.js"); // Import the scraper function
 
 const app = express();
 const PORT = process.env.PORT || 3000;
