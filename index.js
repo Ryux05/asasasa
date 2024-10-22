eab1fe9ff7b12b41c1fd18245bb76418e4476139
yyyyyy <youindo10@gmail.com>
2024-10-22 21:38:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const mmH = require("./mm.js"); // Import the scraper function
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
